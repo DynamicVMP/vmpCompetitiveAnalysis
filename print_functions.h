2d070c514917298174568b5e517abdfff15ca401
szalimben <szalimben@debian>
2016-02-20 16:24:08 -0300

    Print functions - Header - Implementation
    Signed-off-by: Saúl Zalimben <szalimben93@gmail.com>

---
+++ print_functions.h
@@ -0,0 +1,18 @@
+/* 
+ * print_functions.h: Virtual Machine Placement Problem - Print Functions Header
+ * Date: 17-11-2014
+ * Author: Fabio Lopez Pires (flopezpires@gmail.com)
+ * Edited by: Saúl Zalimben 20-02-2016
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+/* print matrix and array */
+void print_int_matrix(int ** matrix, int rows, int columns);
+void print_float_matrix(float ** matrix, int rows, int columns);
+void print_int_array(int * array, int columns);
+void print_float_array(float * array, int columns);
