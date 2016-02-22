e7dcffe61d72e2733d8ddd3ab3677277fff6ce93
Zalimben <szalimben93@gmail.com>
2016-02-21 22:25:19 -0300

    Update the matrix utilization
    Signed-off-by: Zalimben <szalimben93@gmail.com>

--- heuristics.h
+++ heuristics.h
@@ -18,6 +18,9 @@
 
 
 /* get the number of physical and virtual machines */
-int first_fit(float *S, float **utilization, int **placement, int **H, int h_size);
+int first_fit(float *S, float **utilization, int ****placement, int **H, int h_size);
+//int first_fit(float *S, float **utilization,  placement, int **H, int h_size);
 
-bool checkResources(float *request, float *utilization, int *H, int h_size);
+bool check_resources(float *request, float *utilization, int *H, int h_size);
+
+void allocate_VM_to_PM(int ****placement, float *request, int pm);
