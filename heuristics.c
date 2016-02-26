aabdb2e2a668aa378898081c7de2d840ef86b33a
Rodrigo Ferreira <rodrigofepy@gmail.com>
2016-02-26 16:46:04 -0300

    - Replace all the malloc functions with calloc

--- heuristics.c
+++ heuristics.c
@@ -60,42 +60,32 @@ int best_or_worst_fit(bool is_best,float *request, float **utilization, int **pl
 	int iterator_physical;
 	int iterator_scenario;
 	int request_rejected = 0;
-	PM_weight_pair_node* PM_ordered_list = malloc(sizeof(PM_weight_pair_node));
+	PM_weight_pair_node* PM_ordered_list = (PM_weight_pair_node*)calloc(1,sizeof(PM_weight_pair_node));
+	PM_ordered_list->h_index = -1;
 	PM_weight_pair_node* clean_list;
 	float weight_PM = 0.0;
-	
-	// printf("Creating ordered list of PM based on average weight\n");
-	
+
 	for (iterator_physical = 0; iterator_physical < h_size; iterator_physical++) {
 		weight_PM = calculate_weight(utilization, H[iterator_physical], iterator_physical);
-		// printf("Weight calculated for iterator %d\n", iterator_physical);
 		insert_PM_to_ordered_list(is_best, &PM_ordered_list, weight_PM, iterator_physical);
-		// printf("PM %d inserted on ordered list\n",iterator_physical);
 	}
 
-	// printf("Ordered list created\n");
 	clean_list = PM_ordered_list;
 	
 	for (iterator_physical = 0; iterator_physical < h_size; iterator_physical++){
-		// // printf("Checking resources for index %d\n",PM_ordered_list->h_index);
 		if (check_resources(request, utilization[PM_ordered_list->h_index], H[PM_ordered_list->h_index])) {
-			// Allocate the VM into the PM
-			// printf("Allocating VM\n");
 			if(allocate_VM_to_PM(placement, request, PM_ordered_list->h_index)) {
 
 				utilization[PM_ordered_list->h_index][0] += request[4]*request[7]/100;
 				utilization[PM_ordered_list->h_index][1] += request[5]*request[8]/100;
 				utilization[PM_ordered_list->h_index][2] += request[6]*request[9]/100;
 			}
-			// printf("VM allocated %d\n", clean_list->h_index);
-			// free_list(clean_list);
+			 free_list(clean_list);
 			return 1;
 		}
-		// printf("Iterating over the ordered list\n");
 		PM_ordered_list = PM_ordered_list->next;
 	}
-	// printf("VM not allocated\n");
-	// free_list(clean_list);
+	 free_list(clean_list);
 	return 0;
 }
 
@@ -119,7 +109,7 @@ float calculate_weight(float **utilization, int *H, int h_index){
 void insert_PM_to_ordered_list(bool is_best, PM_weight_pair_node** PM_ordered_list, float weight_PM, int h_index) {
 
 	bool ( *comparator )( float, float );
-	PM_weight_pair_node* new_node = malloc(sizeof(PM_weight_pair_node));
+	PM_weight_pair_node* new_node = (PM_weight_pair_node*)calloc(1,sizeof(PM_weight_pair_node));
 
 	new_node->weight = weight_PM;
 	new_node->h_index = h_index;
@@ -131,12 +121,14 @@ void insert_PM_to_ordered_list(bool is_best, PM_weight_pair_node** PM_ordered_li
 		comparator = &worst_comparator;
 	}
 
+	if((*PM_ordered_list)->h_index == -1){
+		*PM_ordered_list = new_node;
+		return;
+	}
+
 	if((*comparator)(new_node->weight,(*PM_ordered_list)->weight)) {
-		// printf("Insert first in list\n");
 		new_node->next = *PM_ordered_list;
 		*PM_ordered_list = new_node;
-		// printf("Index %d\n",new_node->h_index);
-		// printf("Index %d\n",(*PM_ordered_list)->h_index);
 		return;
 	}
 
@@ -144,22 +136,15 @@ void insert_PM_to_ordered_list(bool is_best, PM_weight_pair_node** PM_ordered_li
 	PM_weight_pair_node* actual = parent->next;
 	
 	while(actual != NULL) {
-		// // printf("IN WHILE  Index %d\n", actual->h_index);
 		if((*comparator)(new_node->weight,actual->weight)){
-			// printf("new_node %d\n", new_node->h_index);
 			new_node->next = actual;
 			parent->next = new_node;
-			// printf("new_node %d\n", new_node->h_index);
-			// printf("Insert in list\n");
-			// printf("Index %d\n", actual->h_index);
 			return;
 		}
 		parent = actual;
 		actual = actual->next;
 	}
 	parent->next = new_node;
-	// printf("Insert last in list\n");
-	// printf("Index %d\n",new_node->h_index);
 	return;
 
 }
@@ -174,13 +159,9 @@ bool worst_comparator(float weight_A, float weight_B){
 
 void free_list(PM_weight_pair_node* list_to_free){
 	PM_weight_pair_node* tmp_pointer;
-	// printf("Release memory\n");
 	while(list_to_free != NULL){
 		tmp_pointer = list_to_free;
 		list_to_free = list_to_free->next;
-		// printf("free begin\n");
 		free(tmp_pointer);
-		// printf("free end\n");
 	}
-	// printf("Memory released\n");
 }
