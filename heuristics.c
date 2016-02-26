f2e2ffd3b701276abc20167c03cb6784eda0b233
Rodrigo Ferreira <rodrigofepy@gmail.com>
2016-02-26 16:54:22 -0300

    - Fix logic of the comparators

--- heuristics.c
+++ heuristics.c
@@ -150,11 +150,11 @@ void insert_PM_to_ordered_list(bool is_best, PM_weight_pair_node** PM_ordered_li
 }
 
 bool best_comparator(float weight_A, float weight_B){
-	return weight_A < weight_B;
+	return weight_A > weight_B;
 }
 
 bool worst_comparator(float weight_A, float weight_B){
-	return weight_A > weight_B;
+	return weight_A < weight_B;
 }
 
 void free_list(PM_weight_pair_node* list_to_free){
